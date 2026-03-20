8734e6d316c62e73398ce2a2efd99ae84a214596
Enes Erfidan <enes.erfidan@campus-racing.at>
2026-03-20 14:31:06 +0100

    First implementation Movie Class & Movie Controller

---
+++ src/main/java/at/ac/fhcampuswien/models/Movie.java
@@ -0,0 +1,75 @@
+package at.ac.fhcampuswien.models;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
+public class Movie {
+
+    private UUID id;
+    private String title;
+    private String genre;
+    private int releaseYear;
+
+    public Movie() {
+
+        id = UUID.randomUUID();
+
+    }
+
+    public Movie(String title, String genre, int releaseYear) {
+
+        this.title = title;
+        this.genre = genre;
+        this.releaseYear = releaseYear;
+        id = UUID.randomUUID();
+
+    }
+        //method to generate movies
+    public static List<Movie> generateDummyMovies() {
+        List<Movie> movies = new ArrayList<>();
+        String[] titles = {"Attack of the killer toasters", "The avocado that saved Christmas", "Aliens vs. Grandmas",
+                "Harry Potter and the midlife crisis", "The cat that new too much",
+                "Escape from Monday", "The great coffee", "Dude,who took the Wi-Fi password?",
+                "Pineapple Pizza: a love story","Just one more episode","The last Donut", "Galactic traffic jam",
+                "Matrix reloaded...again", "The data wars","A web page story", "Breaking algorithms",
+                "Bazinga", "The final exam", "2AM study session","Major decisions, minor regrets"};
+
+        String[] genres = {"Action", "Drama", "Sci-Fi", "Comedy",
+                "Thriller", "Horror", "Fantasy", "Adventure", "Romance", "Gay"};
+
+        for (int i = 0; i < titles.length; i++) {
+            movies.add(new Movie(titles[i], genres[i / 2], new Random().nextInt(1960, 2026)));
+        }
+
+        for (Movie movie : movies) {
+            System.out.println(movie);
+        }
+
+        return movies;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{id=" + id + ", title='" + title + "', genre='" + genre + "', releaseYear=" + releaseYear + "}";
+    }
+
+    //getters
+    UUID getID() {
+        return id;
+    }
+
+    String getTitle() {
+        return title;
+    }
+
+    String getGenre() {
+        return genre;
+    }
+
+    int getReleaseYear() {
+        return releaseYear;
+    }
+
+}
