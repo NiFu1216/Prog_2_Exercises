8526b3e91cd50ae773232f92e9af1241e21b6be0
Enes Erfidan <enes.erfidan@campus-racing.at>
2026-04-27 09:49:02 +0200

    Added helper method in ApiUtils as stated in Exercise 2
    Reworked MovieController handle methods
    + unified helpers methods for movie data
    + unified helper for requesting exchange body
    + unified helper method for request action

--- src/main/java/at/ac/fhcampuswien/ApiUtils.java
+++ src/main/java/at/ac/fhcampuswien/ApiUtils.java
@@ -5,6 +5,8 @@ import com.sun.net.httpserver.HttpExchange;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ApiUtils {
     public static void sendResponse(HttpExchange exchange, int statusCode, String response) throws IOException {
@@ -15,5 +17,16 @@ public class ApiUtils {
         OutputStream os = exchange.getResponseBody();
         os.write(bytes);
         os.close();
+    }
+
+    public static Map<String, String> parseQueryParams(String query){
+        Map<String, String> map = new HashMap<>();
+        if(query == null || query.isEmpty()) return map;              //Handling error case if no input
+        String[] pairs = query.split("&");                      //query pairs get split up
+        for(String pair : pairs){
+            String[] kv = pair.split("=", 2);
+            if(kv.length == 2) map.put(kv[0], kv[1]);                 //Only include key:value pairs
+        }
+        return map;
     }
 }
